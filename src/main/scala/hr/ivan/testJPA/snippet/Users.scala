7c293355c12e790b6ffe0bafb256b91f465b0808
unknown <ivans@.in2.hr>
2009-05-12 17:01:41 +0200

    Iskoristen SimpleSifarnik u Useri snippetu

--- src/main/scala/hr/ivan/testJPA/snippet/Users.scala
+++ src/main/scala/hr/ivan/testJPA/snippet/Users.scala
@@ -12,12 +12,12 @@ import javax.persistence.{EntityExistsException,PersistenceException}
 
 import hr.ivan.testJPA.model._
 import hr.ivan.testJPA.dao._
-import hr.ivan.util.{PageUtil, EntityUtil}
+import hr.ivan.util.{PageUtil, EntityUtil, SimpleSifarnik}
 import EntityUtil._
 import PageUtil._
 import Model._
 
-class Users {
+class Users extends SimpleSifarnik[User](new User) {
 
     def list (xhtml : NodeSeq) : NodeSeq = {
         val users = Model.createNamedQuery[User]("findAllUsers") getResultList()
@@ -26,7 +26,7 @@ class Users {
                      "firstName" -> Text(user.firstName),
                      "lastName" -> Text(user.lastName),
                      "ured" -> (if(user.ured != null) Text(user.ured.naziv) else Text("")),
-                     "edit" -> SHtml.link("/users/users", () => userVar(user), Text(?("Edit"))),
+                     "edit" -> SHtml.link("/users/users", () => entityVar(user), Text(?("Edit"))),
                      "listRole" -> user.listRoleUsera.flatMap(rk =>
                         bind("rola", chooseTemplate("user", "listRole", xhtml),
                              "naziv" -> rk.rola.naziv)
@@ -37,16 +37,13 @@ class Users {
         )
     }
 
-    object userVar extends RequestVar(new User())
-    def user = userVar.is
-
     def add (xhtml : NodeSeq) : NodeSeq = {
         def doAdd () = {
-            if (user.lastName.length == 0) {
+            if (entity.lastName.length == 0) {
                 error("lastName", "The users last name cannot be blank")
             } else {
                 try {
-                    Model.mergeAndFlush(user)
+                    Model.mergeAndFlush(entity)
                     redirectTo("/users/users")
                 } catch {
                     case ee : EntityExistsException => logAndError("Author already exists ", ee)
@@ -57,25 +54,25 @@ class Users {
 
         def doDeleteRole(ru : RolaUser) = {
             Log.info("doDeleteRole ru = " + ru)
-            Log.info("doDeleteRole user = " + user)
-            user._listRoleUsera.remove(ru)
+            Log.info("doDeleteRole user = " + entity)
+            entity._listRoleUsera.remove(ru)
         }
 
-        val currentUser = user
+        val currentUser = entity
         val choices = createSelectChoices(Some(?("Odaberite ured...")), UredDAO.allUredi, (ured : Ured) => (ured.id.toString -> ured.naziv))
-        val default = if (user.ured != null) { Full(user.ured.id.toString) } else { Empty }
+        val default = if (entity.ured != null) { Full(entity.ured.id.toString) } else { Empty }
         val choicesRole = createSelectChoices(Some(?("Odaberite rolu...")), RolaDAO.allRoleAktivne, (rola : Rola) => (rola.id.toString -> rola.naziv))
         var odabranaRola : Option[Rola] = None
 
         bind("user", xhtml,
-             "id" -> SHtml.hidden(() => userVar(currentUser)),
-             "firstName" -> SHtml.text(user.firstName, user.firstName = _),
-             "lastName" -> SHtml.text(user.lastName, user.lastName = _),
+             "id" -> SHtml.hidden(() => entityVar(currentUser)),
+             "firstName" -> SHtml.text(entity.firstName, entity.firstName = _),
+             "lastName" -> SHtml.text(entity.lastName, entity.lastName = _),
              "ured" -> SHtml.select(choices, default,
                                     uredId => {
-                    user.ured = getFromEM(classOf[Ured], uredId, Model).getOrElse(null)
+                    entity.ured = getFromEM(classOf[Ured], uredId, Model).getOrElse(null)
                 }),
-             "listRole" -> user.listRoleUsera.flatMap(rk =>
+             "listRole" -> entity.listRoleUsera.flatMap(rk =>
                 bind("rola", chooseTemplate("user", "listRole", xhtml),
                      "naziv" -> rk.rola.naziv,
                      "delete" -> SHtml.submit("Delete", () => {
@@ -85,7 +82,7 @@ class Users {
              "rolaDodaj" -> {SHtml.select(choicesRole, Some(""),
                                           rolaId => { odabranaRola = getFromEM(classOf[Rola], rolaId, Model) }) ++
                              SHtml.submit(?("Dodaj rolu"), () => odabranaRola match {
-                        case Some(rola) => user.addRola(rola)
+                        case Some(rola) => entity.addRola(rola)
                         case None =>
                     } )},
              "submit" -> SHtml.submit(?("Save"), doAdd))
